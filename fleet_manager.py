b8796cc200742b1fcf30216a986e9f934ad883bf
Jack-Vanhecke <jv11@kent.ac.uk>
2026-02-02 21:12:34 +0000

    Added function definitions to fleet_manager.py

--- fleet_manager.py
+++ fleet_manager.py
@@ -1,3 +1,24 @@
+init_database():
+
+display_menu():
+
+add_member(names, ranks, divs, ids):
+
+remove_member(names, ranks, devs, ids):
+
+update_rank(names, ranks, ids):
+
+display_roster(names, ranks, divs, ids):
+
+search_crew(names, ranks, divs, ids):
+
+filter_by_division(names, divs):
+
+claculate_payroll(ranks):
+
+count_officers(ranks):
+
+
 main():
 
   
